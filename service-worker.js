cffe753cd372783181fd6dda89494542d16f0fa0
publicface <ajmr60290@gmail.com>
2026-01-04 09:52:43 +0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", function (event) {
+  console.log("Service Worker Installed");
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    fetch(event.request).catch(function () {
+      return new Response("Offline");
+    })
+  );
+});
